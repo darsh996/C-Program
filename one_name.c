8e938d9b272915d06982c6321142f9fb185e6238
Darshan Vijay Satone <darshansatone1996@gmail.com>
2022-07-17 02:25:54 +0530

    Prac

---
+++ one_name.c
@@ -0,0 +1,10 @@
+// WAP to print a name
+#include<stdio.h>
+
+int main()
+{
+    printf("Welcome to programming practice\n");
+    printf("Darshan\n");
+    printf("Revision\n");
+    printf("Thank you");
+}
